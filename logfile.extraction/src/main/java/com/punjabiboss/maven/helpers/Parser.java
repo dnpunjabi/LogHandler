93f4aa4aad1447bee7ffddc955c585b894837db4
dnpunjabi <40994140+dnpunjabi@users.noreply.github.com>
2022-08-31 23:27:14 +0530

    Initial Commit

---
+++ logfile.extraction/src/main/java/com/punjabiboss/maven/helpers/Parser.java
@@ -0,0 +1,37 @@
+package com.punjabiboss.maven.helpers;
+
+import com.google.gson.Gson;
+import com.punjabiboss.maven.data.models.*;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class Parser {
+    public void parseLogs(BufferedReader reader, DAO dao) throws IOException, SQLException {
+        HashMap<String, ServerLog> eventMap = new HashMap<String, ServerLog>();
+        Gson gson = new Gson();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            ServerLog log = gson.fromJson(line, ServerLog.class);
+            String eventId = log.getId();
+            if (!eventMap.containsKey(eventId)) {
+                eventMap.put(eventId, log);
+                continue;
+            }
+
+            ServerLog previousLog = eventMap.remove(eventId);
+            long duration = Math.abs(log.getTimeStamp() - previousLog.getTimeStamp());
+            boolean alert = false;
+            if (duration > 4) {
+                alert = true;
+            }
+
+            HandleEvent event = new HandleEvent.Builder(eventId, duration, alert)
+                    .withHost(log.getHost())
+                    .withType(log.getType())
+                    .build();
+            dao.writeEvent(event);
+        }
+    }
+}
